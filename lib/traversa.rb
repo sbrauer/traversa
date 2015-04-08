62780d41c8423de54adfc761cc0c9665e8c0877d
Sam Brauer <sbrauer@rentpath.com>
2015-04-08 14:20:24 -0400

    refactor #traverse from recursive to iterative
    It's now more verbose, but it might be easier to understand.
    More importantly, it doesn't mutate the subpath argument.
    I suspect it may be faster too.

--- lib/traversa.rb
+++ lib/traversa.rb
@@ -7,15 +7,18 @@ module Traversa
   # subpath arg should be either an array of strings or a slash-delimited string.
   # In other words, 'foo/bar/baz' and ['foo', 'bar', 'baz'] are equivalent subpath values.
   def self.traverse(resource, subpath)
-    subpath = coerce_subpath(subpath)
-    if resource.respond_to?(:child) && ! subpath.empty?
-      child = resource.child(subpath.first)
-      if child
-        subpath.shift
-        return traverse(child, subpath)
+    unless subpath.empty?
+      subpath = coerce_subpath(subpath)
+      subpath.each_with_index do |name, index|
+        child = get_child(resource, name)
+        if child
+          resource = child
+        else
+          return TraversalResult.new(resource, subpath.slice(index..-1))
+        end
       end
     end
-    TraversalResult.new(resource, subpath)
+    TraversalResult.new(resource, [])
   end
 
   def self.coerce_subpath(subpath)
@@ -24,6 +27,11 @@ module Traversa
     else
       subpath
     end
+  end
+
+  # Return the named child resource or nil.
+  def self.get_child(resource, name)
+    resource.child(name) if resource.respond_to?(:child)
   end
 
   # Returns array of parent resources for the given resource.
